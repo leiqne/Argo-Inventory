cbf65eac5c9ed97386d29e5473094dfd0d15887f
nakato156 <christianvelasces@gmail.com>
2024-12-27 10:28:43 -0500

    add filter for pending state

--- src/routes/routes.py
+++ src/routes/routes.py
@@ -2,7 +2,6 @@ from flask import Blueprint, request, render_template, redirect
 from ..controllers.inventory_controller import leer_inventario, agregar_envase, obtener_nuevo_id, get_csv_cliente
 from datetime import datetime
 
-
 app_router = Blueprint("app_router", __file__)
 
 @app_router.get("/")
@@ -12,7 +11,7 @@ def index():
 
 @app_router.get("/get-pendientes/<string:client_name>")
 def get_peendiente_by_client(client_name:str):
-    data = get_csv_cliente(client_name)
+    data = filter(lambda x: x["estado"] == "pendiente", get_csv_cliente(client_name))
     return render_template('sumary_pend.html', client_name=client_name, paths=[{'name': 'pendientes', 'url':'#'}, {'name': client_name, 'url':f'#{client_name}'}], envases=data, zip=zip)
 
 
